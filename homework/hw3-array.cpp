cca1404275517fcae2e0b2863428e63ab376348b
kornilova <kornilova203@gmail.com>
2017-05-20 12:47:51 +0300

    Added homework information about functions

---
+++ homework/hw3-array.cpp
@@ -0,0 +1,16 @@
+/*
+ * Написать функцию, которая сортирует элементы массива в порядке возрастания
+ */
+
+#include <iostream>
+using namespace std;
+
+void sort(int *array, int n) {
+    // здесь должен быть твой код
+}
+
+int main () {
+    int array[5] = {1, 4, 2, 5, 3};
+    sort(array, 5);
+    return 0;
+}
